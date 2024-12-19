fa633955b2fd9867bafd7a518ac936dad0065c48
坂田 健一 <X1652411@CSSCRESCENT.local>
2024-12-19 16:22:07 +0900

    timer testing

--- many_buttons_arrange_2.py
+++ many_buttons_arrange_2.py
@@ -8,7 +8,10 @@ Press the buttons in the order of the numbers displayed.
 
 import TkEasyGUI as eg
 import random
-import pygame
+# import tkinter as tk 
+
+tk = eg.ttk.Tk()
+
 
 # 1から10 のリストを生成
 numbers = []
@@ -37,9 +40,7 @@ layout.append(
 layout.append(
 [eg.Text("\n")]
 )
-pygame.init()
-# 時計オブジェクトの作成
-clock = pygame.time.Clock()
+
 
 
 # layout.append([eg.Text("\n")])
@@ -60,34 +61,24 @@ window = eg.Window("Many buttons", layout)
 
 line_num = 0
 
-running = True
-start_time = pygame.time.get_ticks()
-print(f"start_time:{start_time}")
-while running:
-    # event loop
-    for event, values in window.event_iter():
-        # close button
-        if event == "Close":
-            running = False
-            break
-        # push number button
-        if event.startswith("-button"):
-            no = window[event].metadata["no"]
-            try:
-                index_num = numbers_q.index(no)
-            except ValueError:
-                index_num = -1
-            if index_num == line_num:
-                line_num += 1
-                window[event].update(disabled=True)
-            if max_num == line_num:
-                eg.popup("Goal !!")
-                break;
-        current_time = pygame.time.get_ticks()
-        print(f"current_time:{current_time}")
-        if current_time - start_time >= 5000:
-            print("5秒経過しました")
-            start_time = current_time
-        clock.tick(60)
+# event loop
+for event, values in window.event_iter():
+    print(f"event:{event}")
+    # close button
+    if event == "Close":
+        break
+    # push number button
+    if event.startswith("-button"):
+        no = window[event].metadata["no"]
+        try:
+            index_num = numbers_q.index(no)
+        except ValueError:
+            index_num = -1
+        if index_num == line_num:
+            line_num += 1
+            window[event].update(disabled=True)
+        if max_num == line_num:
+            eg.popup("Goal !!")
+            break;
 
 window.close()
